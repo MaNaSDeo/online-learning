701b6caf8590e0c9c1a2950b288592f5e5094669
MaNaSDeo <rmnslogin@gmail.com>
2025-08-10 17:23:09 +0530

    fetching particular record

--- udemy/next-js-Stephen-Grider/snippets/src/app/snippets/[id]/page.tsx
+++ udemy/next-js-Stephen-Grider/snippets/src/app/snippets/[id]/page.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { FC } from "react";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 
-const Snippets = (props: any) => {
-  console.log("Snippets", { props });
-  return <div>Snippets</div>;
+interface ISnippetShowPage {
+  params: {
+    id: string;
+  };
+}
+
+const Snippets: FC<ISnippetShowPage> = async (props) => {
+  const { params } = props;
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(params.id, 10) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+  return <div>{snippet.title}</div>;
 };
 
 export default Snippets;
