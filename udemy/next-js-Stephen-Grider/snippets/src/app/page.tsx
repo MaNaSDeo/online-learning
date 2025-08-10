846ffe30f67c44ae475d7a98e46d4eda074be025
MaNaSDeo <rmnslogin@gmail.com>
2025-08-10 16:36:20 +0530

    fetch data with server components

--- udemy/next-js-Stephen-Grider/snippets/src/app/page.tsx
+++ udemy/next-js-Stephen-Grider/snippets/src/app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+import { db } from "@/db";
 
-export default function Home() {
-  return <div>Base setup</div>;
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+  console.log("home snippets", snippets);
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+  return <div>{renderedSnippets}</div>;
 }
+
+/**
+ * Target - Fetching data
+ * - create a server compoenent -> a component that doesn't have 'use client' at the top, is automatically a server component.
+ * - Mark the component as 'async'
+ * - Make an HTTP request or directly access a database to get our data.
+ * - Render our data correctly, or pass it to a child component
+ */
