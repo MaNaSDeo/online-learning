fec299ed2be4822645eded41af0e557db2ff2cde
MaNaSDeo <rmnslogin@gmail.com>
2025-08-10 17:59:18 +0530

    feat: Snippet UI

--- udemy/next-js-Stephen-Grider/snippets/src/app/snippets/[id]/page.tsx
+++ udemy/next-js-Stephen-Grider/snippets/src/app/snippets/[id]/page.tsx
@@ -21,7 +21,21 @@ const Snippets: FC<ISnippetShowPage> = async (props) => {
   if (!snippet) {
     return notFound();
   }
-  return <div>{snippet.title}</div>;
+  const { id, title, code } = snippet;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{code}</code>
+      </pre>
+    </div>
+  );
 };
 
 export default Snippets;
